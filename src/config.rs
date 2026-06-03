8e86db6a7d322a7275e9f4b56f42a32bb3012144
nico359 <124350776+nico359@users.noreply.github.com>
2026-06-03 21:45:16 +0200

    Bump version to 0.3.0

--- src/config.rs
+++ src/config.rs
@@ -1,4 +1,4 @@
-pub static VERSION: &str = "0.2.0";
+pub static VERSION: &str = "0.3.0";
 pub static GETTEXT_PACKAGE: &str = "speedometer";
 pub static LOCALEDIR: &str = "/app/share/locale";
 pub static PKGDATADIR: &str = "/app/share/speedometer";
